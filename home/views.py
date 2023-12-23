a7499fbecddfad6c7c935249998cd3081dea8ec9
mahfujur <mahfujul.hoque@heliosinsider.com>
2023-12-23 12:24:23 +0600

    fixed-map-issue

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.conf import settings
 
 # Create your views here.
 
@@ -7,3 +9,13 @@ def index(request):
 
     # Page from the theme 
     return render(request, 'pages/index.html')
+
+
+@login_required(login_url='/accounts/login-v1/')
+def google_maps(request):
+  context = {
+    'parent': 'maps',
+    'segment': 'google_maps',
+    'api_key' : getattr(settings, 'GOOGLE_MAP_API_KEY')
+  }
+  return render(request, 'pages/chart-maps/map-google.html', context)
