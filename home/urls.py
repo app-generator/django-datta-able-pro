ccaf5c031b7cbd07171843b3584816fe060e4348
App Generator <51070104+app-generator@users.noreply.github.com>
2024-02-10 15:45:17 +0200

    CleanUp before Update

--- home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('maps/google-maps/', views.google_maps, name="google_maps"),
-]
